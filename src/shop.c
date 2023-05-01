4fa32aa2fa780932645da9944173268daca96b14
jorgem141 <jorgem141@outlook.com>
2023-04-30 20:06:25 -0400

    Make TMs reusable and unsellable.

--- src/shop.c
+++ src/shop.c
@@ -997,6 +997,11 @@ static void Task_BuyMenu(u8 taskId)
             else if (!IsEnoughMoney(&gSaveBlock1Ptr->money, sShopData->totalCost))
             {
                 BuyMenuDisplayMessage(taskId, gText_YouDontHaveMoney, BuyMenuReturnToItemList);
+
+            }
+            else if (ItemId_GetPocket(itemId) == POCKET_TM_HM && CheckBagHasItem(itemId, 1))            
+            {
+                BuyMenuDisplayMessage(taskId, gText_YouAlreadyHaveThis, BuyMenuReturnToItemList);
             }
             else
             {
