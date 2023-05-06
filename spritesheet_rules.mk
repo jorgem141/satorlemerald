69ff1ac5e06095785d57b82216ef7f0d236a4825
jorgem141 <jorgem141@outlook.com>
2023-05-06 14:41:36 -0400

    Added Juju sprite rule

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -727,3 +727,6 @@ $(OBJEVENTGFXDIR)/people/gym_leaders/kipp.4bpp: %.4bpp: %.png
 
 $(OBJEVENTGFXDIR)/people/gym_leaders/xin.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
+
+$(OBJEVENTGFXDIR)/people/gym_leaders/juju.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -mwidth 2 -mheight 4
